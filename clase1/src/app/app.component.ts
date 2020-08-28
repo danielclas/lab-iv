74daac2bb835a62b160b629ad211513976367cc0
Daniel clas <dclas@santandertecnologia.com.ar>
2020-08-27 22:24:59 -0300

    Clase 1

--- clase1/src/app/app.component.ts
+++ clase1/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { parse } from 'path';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clase1';
+  
+  edad1: any;
+  edad2: any;
+  resultado: any;
+
+  calcular(){
+    let num1 = parseFloat(this.edad1);
+    let num2 = parseFloat(this.edad2);
+
+    if(num1 && num2){
+      this.resultado = (num1 + num2) / 2;
+    }else{
+      alert("Valores ingresados no son numéricos");
+      this.limpiar();
+    }
+  }
+
+  limpiar(){
+    this.edad1 = "";
+    this.edad2 = "";
+    this.resultado = "";
+  }
 }
--- clase1/src/app/app.module.ts
+++ clase1/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { ErrorComponent } from './componentes/error/error.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BienvenidoComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
