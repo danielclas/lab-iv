676538fa438949d31990599134519a431d8c2c23
danielclas <danielclas@outlook.es>
2020-10-08 18:36:47 -0300

    Componente de parcial

---
+++ src/app/parcial/home-parcial/home-parcial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-parcial',
+  templateUrl: './home-parcial.component.html',
+  styleUrls: ['./home-parcial.component.css']
+})
+export class HomeParcialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
